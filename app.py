ece88d840252abdfe0010faea57cefd232dc39d9
Eyuwankg-Swankg <messikarthik13@gmail.com>
2020-12-20 14:18:02 +0530

    added routes to add todo

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
- 
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/add", methods=["POST", "GET"])
+def addTodo():
+    if request.method == "POST":
+        print(request.form["todo"])
+        return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
