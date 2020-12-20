14d79cf15ea79e39bce29eb9ce6ec3c4efaafe7a
Eyuwankg-Swankg <messikarthik13@gmail.com>
2020-12-20 16:42:24 +0530

    added SQLAlchemy DB

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todos.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class todos(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    todo = db.Column(db.String(100))
+
+    def __init__(self, todo):
+        self.todo = todo
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    # GET TODO FROM DB
+    todoFromDB = db.session.query(todos).all()
+    todosList = []
+    for todo in todoFromDB:
+        todosList.append(todo.todo)
+    return render_template("home.html", todos=todosList)
 
 
-@app.route("/add", methods=["POST", "GET"])
+@app.route("/add", methods=["POST"])
 def addTodo():
-    if request.method == "POST":
-        print(request.form["todo"])
-        return redirect(url_for("home"))
+    todo = request.form["todo"]
+    print(f"------------------------\n{todo}\n------------")
+    if len(todo) > 3:
+        todoOB = todos(todo)
+        db.session.add(todoOB)
+        db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
